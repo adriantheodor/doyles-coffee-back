efb18766b720afc032084ddc537fe567e82b10d8
adriantheodor <aatheodor@gmail.com>
2025-11-20 14:35:18 -0500

    deployment

--- server.js
+++ server.js
@@ -22,10 +22,24 @@ if (!process.env.MONGO_URI) {
 }
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.use(helmet());
-app.use(cors());
+
+
+app.use(
+  cors({
+    origin: [
+      "http://doyles-coffee-front.vercel.app/",
+      "http://www.doylesbreakroomservices.com/",
+      "http://doylesbreakroomservices.com/",
+    ],
+    credentials: true,
+  })
+);
+
+
+
 app.use(express.json());
 
 // Routes
